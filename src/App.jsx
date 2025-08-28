c280fd2caf6f9126cc69b5a92085f2d18f7d0e3b
“BlackBlossom” <vikingpanther72@gmail.com>
2025-08-28 22:45:49 +0530

    Added new files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='flex items-center justify-center h-screen'>
+        <h1 className="text-3xl font-bold underline">
+          Hello React!
+        </h1>
+    </div>
   )
 }
 
